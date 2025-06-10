8f6accb007828d26680f476af9fdd92e0f64b91a
szylinski-sa <simeon.zylinski@sa.gov.au>
2025-06-10 13:23:00 +0930

    Argument `...` for taskbutton_dynamic to take more style options

--- R/taskbutton_dynamic.R
+++ R/taskbutton_dynamic.R
@@ -7,12 +7,14 @@
 #'
 #' @param id inputID to track input/action (as any other shiny input).
 #' @param label_ready Label to display before clicking i.e. ready state of input_task_button.
-#' @param loaded #Logical; how to track loaded status (usually a reactiveValues value).
-#' @param changed #Optional logical; how to track if the button should be clickable again, when `loaded==TRUE`.
-#' @param label_busy Label to display when processing. Used directly in same argument of `input_task_button.`
+#' @param loaded Logical; how to track loaded status (usually a reactiveValues value).
+#' @param changed Optional logical; how to track if the button should be clickable again, when `loaded==TRUE`.
+#' @param label_busy Label to display when processing. Used directly in same argument of `input_task_button.`.
+#' @param label_done Label to display when process is finished. Displayed on a disable `actionButton`.
 #' @param label_changed Label to display when `loaded==TRUE` but new action is available and button is clickable again.
 #' @param class Sent to `class` argument of all buttons (e.g. 'btn-sm')
 #' @param type Sent to `type` argument of input_task_buttons (default 'primary')
+#' @param ... Sent to all buttons; Named arguments become attributes to include on the <button> element.
 #'
 #' @returns An `input_task_button`, or a disabled `actionButton` when action is complete.
 #'
@@ -86,14 +88,16 @@ taskbutton_dynamic <- function(id,
                                label_done = "Loaded",
                                label_changed = "Update",
                                class = NULL,
-                               type = 'primary'
+                               type = 'primary',
+                               ...
 ) {
 
   ui <- input_task_button(id = id,
                           label = label_ready,
                           label_busy = label_busy,
                           type = type,
-                          class = class)
+                          class = class,
+                          ...)
 
   if(loaded){
     ui <- actionButton(inputId = paste0(id, "_inop"), #dummy name to ensure its not listened to
@@ -109,11 +113,8 @@ taskbutton_dynamic <- function(id,
                             label = label_changed,
                             label_busy = label_busy,
                             type = type,
-                            class = class)
-    # ui <- actionButton(inputId = id,
-    #                    label = label_changed,
-    #                    icon = NULL,
-    #                    )
+                            class = class,
+                            ...)
   }
 
   ui
